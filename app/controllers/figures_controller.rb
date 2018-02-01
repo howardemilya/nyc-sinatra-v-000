d2eb6c23f3cac8784d32f5cada646dcfd3f2a5b8
Emily Howard <howard.emilya@gmail.com>
2018-02-01 06:48:56 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,56 @@
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+
+    erb :'figures/new'
+  end
+
+  get '/figures' do
+
+    erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/edit'
+  end
+
+
+  post '/figures' do
+
+    @figure = Figure.find_or_create_by(name: params[:figure]["name"])
+
+    if params[:figure]["title_ids"]
+      params[:figure]["title_ids"].each do |id|
+        @figure.titles << Title.find_by_id(id)
+      end
+    end
+
+    if !params[:title]["name"].empty?
+      @figure.titles << Title.find_or_create_by(params[:title])
+    end
+
+    if params[:figure]["landmark_ids"]
+      params[:figure]["landmark_ids"].each do |id|
+        @figure.landmarks << Landmark.find_by_id(id)
+      end
+    end
+
+
+    if !params[:landmark]["name"].empty?
+      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+
+  end
+
+end
